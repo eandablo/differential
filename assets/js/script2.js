232675031036c294f35eae63fdb5165048344d6f
eandablo <>
2023-07-26 11:19:23 +0000

    add js code in script2 to deal with suscription details and pass then to local storage

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,11 +1,28 @@
-document.addEventListener('DOMContentLoaded', activateButtons);
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('suscribe-button').addEventListener('click', loginFunction);
+});
+
 /**
- * Reads the id and decides action depending on the event id
+*Reads info from suscribe form 
+*if criteria are satisfied logs user in with its newly created account
  */
-function activateButtons() {
-    document.getElementById('suscribe-button').addEventListener('click', loginFunction);
-}
 function loginFunction(event) {
-    event.preventDefault();
+  event.preventDefault();
+  let usersInfo = ['e@gmail.com','l@gmail.com'];
+  let userName=document.getElementById('fname').value;
+  let userDegree=document.getElementById('education').value;
+  let userEmail=document.getElementById('user-email').value;
+  let userExist=usersInfo.includes(userEmail);
+  if (userExist){
+    alert('Email already in use');
+    document.getElementById('fname').value="";
+    document.getElementById('education').value="";
+    document.getElementById('user-email').value="";
+    document.getElementById('user-pass').value="";
+  } else{
+    localStorage.setItem('useremail', userEmail);
+    localStorage.setItem('username', userName);
+    localStorage.setItem('userdegree', userDegree);
     window.location.href="userpage.html";
+  }
 }
