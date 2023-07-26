a89a94959217d5c32f56d31fdf78e933a3be2b1e
eandablo <>
2023-07-26 14:33:24 +0000

    add user info button to userpage

--- assets/js/script-u.js
+++ assets/js/script-u.js
@@ -1,11 +1,17 @@
 /**
  * Reads user information gerated in either login or suscribe pages
- * uses the information to create the personalised user space
+ * using the information to create the personalised user space
  */
-
-//function loginInfo(){
+function loginInfo(){
   let userName=localStorage.getItem('username');
   let userEmail=localStorage.getItem('useremail');
   let userDegree= localStorage.getItem('userdegree');
-  document.getElementById('user-first-name').innerText=userName.toUpperCase();
-//}
+  if (userName.includes(' ')){
+    let indexFirstSpace=userName.indexOf(' ');
+    let firstName=userName.slice(0,indexFirstSpace);
+    document.getElementById('user-first-name').innerText=firstName.toUpperCase();
+  }else{
+    document.getElementById('user-first-name').innerText=userName.toUpperCase();
+  }
+}
+loginInfo();
