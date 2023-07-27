cfce02c3aba7ec5fab9686b95439bb58f648decf
eandablo <>
2023-07-27 10:56:22 +0000

    add functionaly to user-button and add information to use-info-div

--- assets/js/script-u.js
+++ assets/js/script-u.js
@@ -6,6 +6,9 @@ function loginInfo(){
   let userName=localStorage.getItem('username');
   let userEmail=localStorage.getItem('useremail');
   let userDegree= localStorage.getItem('userdegree');
+  document.getElementById('user-name').innerText=userName;
+  document.getElementById('user-email').innerText=userEmail;
+  document.getElementById('user-education').innerText=userDegree;
   if (userName.includes(' ')){
     let indexFirstSpace=userName.indexOf(' ');
     let firstName=userName.slice(0,indexFirstSpace);
@@ -15,3 +18,16 @@ function loginInfo(){
   }
 }
 loginInfo();
+document.addEventListener('DOMContentLoaded',function(){
+    let infoButton = document.getElementById('user-button');
+    infoButton.addEventListener('click',infoDisplayButton);
+});
+function infoDisplayButton(event){
+    let infoDisplay = document.getElementById('user-info-div');
+    if (window.getComputedStyle(infoDisplay).display==='none') {
+    console.log(window.getComputedStyle(infoDisplay).display);
+      infoDisplay.style.display='block';
+    } else{
+      infoDisplay.style.display = 'none';
+    }
+}
