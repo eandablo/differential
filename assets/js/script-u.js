7ca2946a0c0f0866a1525502b8b62a483f1dcd1e
eandablo <>
2023-07-28 13:50:49 +0000

    add function addDissipator and code

--- assets/js/script-u.js
+++ assets/js/script-u.js
@@ -19,14 +19,13 @@ function loginInfo(){
 }
 // call function to load user info
 loginInfo();
-// clear user info from the local storage
 document.addEventListener('DOMContentLoaded',function(){
   let infoButton = document.getElementById('user-button');
   let springButton=document.getElementById('spring-button');
-//  let dissipatorButton=document.getElementById('dissipator-button');
+  let dissipatorButton=document.getElementById('dissipator-button');
   infoButton.addEventListener('click',infoDisplayButton);
   springButton.addEventListener('click',addSpring);
- // dissipatorButton.addEventListener('click',function);
+  dissipatorButton.addEventListener('click',addDissipator);
 });
 /**
  * shows or hides the information when the burger button is pressed
@@ -43,30 +42,59 @@ function infoDisplayButton(){
  * Adds a new sring element
  */
 function addSpring(){
-  let nsprings=1;
   if (document.getElementsByClassName('spring-div')[0]){
     let nSprings= document.getElementsByClassName('spring-div').length+1;
     //stops producing elements if there are already 3 springs or total elements are 4
     let nDissipator=0;
     if (document.getElementsByClassName('dissipator-div')[0]){
-      nDissipator = document.getElementsByClassName('dissipator-div');
+      nDissipator = document.getElementsByClassName('dissipator-div').length;
     }
-    let tElements=nSprings+nDissipator;
+    let tElements=nSprings+nDissipator-1;
     if (nSprings<=3 && tElements<4){
       let newSpring = document.createElement("div");
       newSpring.setAttribute('class', 'spring-div');
       newSpring.setAttribute('id', 'spring-' + nSprings);
       document.getElementById('game-area-left').appendChild(newSpring);
     } else if (tElements < 4){
-      alert('There are already 3 spring elements, you need at least 1 dissipator');
+      alert(`Total of springs is 3 you need at least one dissipator`);
     } else{
-      alert('You have exceeded the maximum number of elements');
+      alert(`number of elements is ${tElements}`);
     }  
   } else {
     let newSpring = document.createElement("div");
     newSpring.setAttribute('class', 'spring-div');
-    newSpring.setAttribute('id','spring-'+nsprings);
+    newSpring.setAttribute('id','spring-1');
     document.getElementById('game-area-left').appendChild(newSpring);
   }
 
+}
+/**
+ * Adds a new dissipator element
+ */
+function addDissipator() {
+  let nDissipator = 1;
+  if (document.getElementsByClassName('dissipator-div')[0]) {
+    let nDissipator = document.getElementsByClassName('dissipator-div').length + 1;
+    //stops producing elements if there are already 3 springs or total elements are 4
+    let nSprings = 0;
+    if (document.getElementsByClassName('spring-div')[0]) {
+      nSprings = document.getElementsByClassName('spring-div').length;
+    }
+      let tElements = nSprings + nDissipator-1;
+      if (nDissipator <= 3 && tElements < 4) {
+        let newDissipator = document.createElement("div");
+        newDissipator.setAttribute('class', 'dissipator-div');
+        newDissipator.setAttribute('id', 'dissipator-' + nDissipator);
+        document.getElementById('game-area-left').appendChild(newDissipator);
+      } else if (tElements < 4) {
+        alert('There are already 3 dissipator elements, you need at least 1 dissipator');
+      } else {
+        alert('You have exceeded the maximum number of elements');
+      }
+  } else {
+    let newDissipator = document.createElement("div");
+    newDissipator.setAttribute('class', 'dissipator-div');
+    newDissipator.setAttribute('id', 'dissipator-' + nDissipator);
+    document.getElementById('game-area-left').appendChild(newDissipator);
+  }
 }
