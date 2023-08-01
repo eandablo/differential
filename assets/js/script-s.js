4b2049032368dac7debb1a5baab52e9789952402
eandablo <>
2023-08-01 12:12:38 +0000

    eliminatesubmit function and enclose it in the addeventlistener

--- assets/js/script-s.js
+++ assets/js/script-s.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
-  document.getElementById('suscribe-button').addEventListener('click', suscribeFunction);
+    document.getElementById('suscribe-button').addEventListener('click', function(event) {
+        event.preventDefault();
+        let usersInfo = ['e@gmail.com', 'l@gmail.com'];
+        let userName = document.getElementById('fname').value;
+        let userDegree = document.getElementById('education').value;
+        let userEmail = document.getElementById('user-email').value;
+        let userExist = usersInfo.includes(userEmail);
+        if (userExist) {
+            alert('Email already in use');
+            document.getElementById('fname').value = "";
+            document.getElementById('education').value = "";
+            document.getElementById('user-email').value = "";
+            document.getElementById('user-pass').value = "";
+        } else {
+            localStorage.setItem('useremail', userEmail);
+            localStorage.setItem('username', userName);
+            localStorage.setItem('userdegree', userDegree);
+            window.location.href = "userpage.html";
+        }
+    });
 });
 
 /**
